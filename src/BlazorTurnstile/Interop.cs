069f8c8039d2525585300ccccc97443515cefecf
Michel Jansson <mail@micheljansson.se>
2023-12-09 11:14:54 +0100

    🩹 Properly remove widget on dispose + simplified interop
    - Capture the widget id on render.
    - Remove widget using widget id on dispose.
    - Use widget id for reset so only reset the current widget.
    - Simplified interop with less js by calling the turnstile api directly.

--- src/BlazorTurnstile/Interop.cs
+++ src/BlazorTurnstile/Interop.cs
@@ -7,21 +7,23 @@ namespace BlazorTurnstile;
 
 internal class Interop : IAsyncDisposable
 {
-    private readonly Lazy<Task<IJSObjectReference>> _moduleTask;
-
 #if NET8_0_OR_GREATER
     private static readonly JsonNamingPolicy _kebabCaseLowerNamingPolicy = JsonNamingPolicy.KebabCaseLower;
 #else
     private static readonly JsonNamingPolicy _kebabCaseLowerNamingPolicy = new JsonKebabCaseLowerNamingPolicy();
 #endif
 
+    private readonly Lazy<Task<IJSObjectReference>> _moduleTask;
+    private readonly IJSRuntime _js;
+
     public Interop(IJSRuntime jsRuntime)
     {
+        _js = jsRuntime;
         _moduleTask = new(() => jsRuntime.InvokeAsync<IJSObjectReference>(
             "import", "./_content/BlazorTurnstile/interop.js").AsTask());
     }
 
-    public async ValueTask RenderAsync(
+    public async ValueTask<string> RenderAsync(
         DotNetObjectReference<Turnstile> component,
         ElementReference widgetElement,
         TurnstileParameters parameters)
@@ -36,14 +38,12 @@ internal class Interop : IAsyncDisposable
         };
         var jsonElement = JsonSerializer.SerializeToElement(parameters, options);
 
-        await module.InvokeVoidAsync("render", component, widgetElement, jsonElement);
+        return await module.InvokeAsync<string>("render", component, widgetElement, jsonElement);
     }
 
-    public async ValueTask ResetAsync()
-    {
-        var module = await _moduleTask.Value;
-        await module.InvokeVoidAsync("reset");
-    }
+    public ValueTask ResetAsync(string widgetId) => _js.InvokeVoidAsync("turnstile.reset", widgetId);
+
+    public ValueTask RemoveAsync(string widgetId) => _js.InvokeVoidAsync("turnstile.remove", widgetId);
 
     public async ValueTask DisposeAsync()
     {
--- src/BlazorTurnstile/wwwroot/interop.js
+++ src/BlazorTurnstile/wwwroot/interop.js
@@ -1,11 +1,6 @@
-
 export function render(obj, element, parameters) {
     parameters['callback'] = (token) => obj.invokeMethodAsync('Callback', token);
     parameters['error-callback'] = (message) => obj.invokeMethodAsync('ErrorCallback', message);
 
-    turnstile.render(element, parameters);
-}
-
-export function reset() {
-    turnstile.reset();
+    return turnstile.render(element, parameters);
 }
